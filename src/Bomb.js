642651d10fbafe464af2498de11290a39a0a1238
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-01 20:18:35 -0800

    commit tree 2a8b43281abb4b24df9b48ce57b062e45911e48c

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,16 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+  render() {
+    return this.state.secondsLeft > 0
+      ? `${this.state.secondsLeft} seconds left before I go boom!`
+      : "Boom!";
+  }
+}
